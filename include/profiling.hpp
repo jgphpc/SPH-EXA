0dfcae14bb5c41e3424233fac6bf7891ce717090
Jean-guillaume Piccinali <piccinali1@jwlogin24.juwels>
2022-02-10 19:57:19 +0100

    ok

--- include/profiling.hpp
+++ include/profiling.hpp
@@ -1,11 +1,35 @@
 #pragma once
 
-#ifdef USE_PROFILING
+#ifdef USE_PROFILING_NVTX
 #include <nvToolsExt.h>
 // std::cout << "USE_PROFILING is ON\n";
-#define MARK_BEGIN( x ) nvtxRangePush(x);
+#define MARK_BEGIN( xx ) nvtxRangePush(xx);
 #define MARK_END nvtxRangePop();
-#else
+#endif
+
+#ifdef USE_PROFILING_SCOREP
+#include "scorep/SCOREP_User.h"
+/* scorep --user
+//no SCOREP_USER_REGION_DEFINE( solve )
+//no ...
+//no SCOREP_USER_REGION_BEGIN( solve, "xx", SCOREP_USER_REGION_TYPE_LOOP )
+//no for (i = 0; i < 100; i++) { [...] }
+//no SCOREP_USER_REGION_END( solve )
+# -------- or:
+{SCOREP_USER_REGION( "xx", SCOREP_USER_REGION_TYPE_LOOP )    <------
+for (i = 0; i < 100; i++) { [...] } 
+}                                                            <------
+# --------
+# SCOREP_RECORDING_OFF() // must be _after_ mpi_init
+# NOTE: SCOREP_RECORDING_OFF() before mpi_finalize will report
+# negative values (wrong): mpi_finalize must be profiled/traced.
+*/
+#define MARK_BEGIN( xx ) {SCOREP_USER_REGION(xx, SCOREP_USER_REGION_TYPE_COMMON)
+#define MARK_END }
+// SCOREP_RECORDING_ON()
+#endif
+
+#ifndef USE_PROFILING_SCOREP
 #define MARK_BEGIN( x )
 #define MARK_END
 #endif
