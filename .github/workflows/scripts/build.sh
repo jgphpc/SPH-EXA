210bb1d526397ef389b6f539a8cf58f7443017a4
jgp <jgp@cscs.ch>
2023-02-23 07:08:21 +0100

    script12

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -51,7 +51,7 @@ cmake \
 #    -t sphexa-cuda -j 4
 #    -j `grep processor /proc/cpuinfo | wc -l`
 
-ls -l ./build/main/src/sphexa/sphexa*
+ls -l /usr/local/games/cuda/build/main/src/sphexa/
 
 # -> ./build/main/src/sphexa/sphexa-cuda
 # TODO: cublas_v2.h
