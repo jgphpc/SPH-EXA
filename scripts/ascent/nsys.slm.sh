11ba68cccb3508c557aeebb049194a45527f509f
jg <jg>
2025-02-25 09:40:54 +0100

    nsys

--- scripts/ascent/nsys.slm.sh
+++ scripts/ascent/nsys.slm.sh
@@ -1,8 +1,9 @@
-for nnn in 1 ;do
-    for sss in 50 ;do
+for nnn in 1 2 4 8 16 32 64 128 ;do
+    for sss in 50 100 200 ;do
         mkdir -p cn$nnn-size$sss
         cd cn$nnn-size$sss
-        sed -e "s-NNN-$nnn-" -e "s-SSS-$sss-" ../nsys.slm.in > nsys.slm
+        s=`echo $nnn $sss | awk '{print $1*$2}'`
+        sed -e "s-NNN-$nnn-" -e "s-SSS-$s-" ../nsys.slm.in > $nnn.$sss.nsys.slm
         sbatch nsys.slm
         cd ..
     done
--- scripts/ascent/nsys.slm.sh.res1.sh
+++ scripts/ascent/nsys.slm.sh.res1.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # for dd in cn32-size* ;do cd $dd ; ./nsys.slm.sh.res1.sh 1 ;cd ..;done
-# for dd in cn32-size* ;do cd $dd ; ./nsys.slm.sh.res1.sh 2 ;cd ..;done |sort -t, -nk 2
+# for dd in cn32-size* ;do cd $dd ; ./nsys.slm.sh.res1.sh 2 ;cd ..;done \
+# |sort -t, -nk 2 |awk -F, '{printf "%s,",$3}'
 
 part=$1
 NSYS=/capstor/scratch/cscs/piccinal/daint/nvidia/nsight-systems-2025.1.1/bin/nsys
 SQLT=/capstor/scratch/cscs/piccinal/daint/nvidia/nsight-systems-2025.1.1/target-linux-sbsa-armv8/sqlite3
 
 if [ $part = "1" ] ;then
+
+# nsys-rep2sqlite:    
 for ii in ascent*.nsys-rep ;do $NSYS export -t sqlite -f on ./$ii ;echo $ii; done
 
+# sqlite2csv:
 for ii in *.sqlite ;do
     echo 'nsec|deviceId|bytes|memKind|memoryOperationType' > $ii.csv;
     $SQLT -readonly $ii -cmd 'select start,deviceId,bytes,memKind,memoryOperationType from CUDA_GPU_MEMORY_USAGE_EVENTS' '.exit' >> $ii.csv
     echo $ii
 done
 
+# incremental memory usage:
 for ii in ascent*.csv ;do
     grep -v nsec $ii |awk -F\| 'BEGIN{s=0}{if ($5==0) s=s+$3; else s=s-$3;print s;}' > $ii.csv
     echo $ii
