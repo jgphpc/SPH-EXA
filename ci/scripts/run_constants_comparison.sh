823e76f49e1870f0ea294f7d86d546b82fe20818
jgphpc <jgp@cscs.ch>
2026-07-10 17:20:38 +0200

    3

--- ci/scripts/run_constants_comparison.sh
+++ ci/scripts/run_constants_comparison.sh
@@ -97,25 +97,21 @@ for ic in "${ics[@]}"; do
 
   if [ "$rank_id" -eq 0 ]; then
 
-    ls -lrt ci/scripts/compare_constants.py
-    ls -lrt ci/reference/const-${ic}-${backend}-rel-ref.txt
-    ls -lrt constants.txt
+    touch ci/reference/const-${ic}-${backend}-rel-ref.txt constants.txt
     abs_cols="${abs_columns_for_ic[$ic]:-}"
     cmd="$uenv_python ci/scripts/compare_constants.py $PWD/ci/reference/const-${ic}-${backend}-rel-ref.txt $PWD/constants.txt"
 
     if [ -n "$abs_cols" ]; then
       cmd+=("$abs_cols")
     fi
-    echo "cmd == $cmd == ${cmd[@]}"
 
-    pwd
-    ${cmd[@]} ; rc=$?
-    echo "rc=$rc"
-    failed_comparisons+=("$ic")
+#    ${cmd[@]} ; rc=$?
+#    echo "rc=$rc"
+#    failed_comparisons+=("$ic")
 
-    # if ! "${cmd[@]}"; then
-    #   failed_comparisons+=("$ic")
-    # fi
+    if ! "${cmd[@]}"; then
+      failed_comparisons+=("$ic")
+    fi
 
   fi
   wait
